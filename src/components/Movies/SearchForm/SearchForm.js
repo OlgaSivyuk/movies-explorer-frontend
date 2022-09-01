2986de6c228734c536ade65bcfd7e4a3db419346
OlgaSivyuk <olchik0308@yandex.ru>
2022-09-01 12:23:46 +0400

    add functionality tumbler off/on

--- src/components/Movies/SearchForm/SearchForm.js
+++ src/components/Movies/SearchForm/SearchForm.js
@@ -1,22 +1,38 @@
+import React, { useState } from 'react';
 import './SearchForm.css';
 
 function SearchForm() {
+  // проверка работы тумблера
+  const [tumbOff, setTumbOff] = useState(false);
+
+  function handleTumbOff() {
+    setTumbOff(!tumbOff);
+  }
+
   return (
     <section className='search'>
-        <label className='search-movie'>
-          <input
-            className='search-movie__input'
-            type='text'
-            placeholder={`Фильм`}
-            required
-          />
-          <button className='search-movie__button' type='button'>Найти</button>
-        </label>
+      <label className='search-movie'>
+        <input
+          className='search-movie__input'
+          type='text'
+          placeholder={`Фильм`}
+          required
+        />
+        <button className='search-movie__button' type='button'>
+          Найти
+        </button>
+      </label>
 
-        <label className='checkbox'>  
-          <p className='checkbox__title'>Короткометражки</p>
-          <span className='checkbox__tumb'></span>
-        </label>
+      <label className='checkbox'>
+        <p className='checkbox__title'>Короткометражки</p>
+        <button
+          className={`checkbox__tumb checkbox__tumb_type_active ${
+            tumbOff ? 'checkbox__tumb_type_off' : ''
+          }`}
+          type='button'
+          onMouseDown={handleTumbOff}
+        ></button>
+      </label>
     </section>
   );
 };
